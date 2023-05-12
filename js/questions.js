08b263de8eda7027d4b8447adc0c81468f640800
PARRAAAA <123692126+PARRAAAA@users.noreply.github.com>
2023-05-12 13:45:43 -0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Who is teachers favorite student?",
+    answer: "Carlos",
+    options: [
+      "Alex",
+      "Carlos",
+      "Franisco",
+      "Gabriel"
+    ]
+  },
+    {
+    numb: 2,
+    question: "what is alex's favorite food? ",
+    answer: "Pizza",
+    options: [
+      "Pizza",
+      "Tacos",
+      "Hamburguer",
+      "hot dog"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what is Carlo's favorite color?",
+    answer: "Green",
+    options: [
+      "Black",
+      "White",
+      "Green",
+      "Blue"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How much is pi exactly?",
+    answer: "3.14",
+    options: [
+      "45.15",
+      "3.14",
+      "34.15",
+      "3.19"
+    ]
+  },
+    {
+    numb: 5,
+    question: "who's the richest person in the world?",
+    answer: "Bernard Arnault",
+    options: [
+      "Elon Musk",
+      "Bernard Arnault",
+      "Larry Ellison",
+      "Carlos"
+    ]
+  },
+
+];
